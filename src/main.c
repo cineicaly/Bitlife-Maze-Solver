9fd46f62d4e5487cd254e9457a91f4e739b51aa3
cineicaly <darius.cipleu@student.upt.ro>
2026-06-29 19:58:56 +0300

    added read_file() and sample maze template

--- src/main.c
+++ src/main.c
@@ -10,6 +10,62 @@
 #define DEBUG_TEST 0
 #endif
 
+char *read_file(char *file_path)
+{
+    FILE *fp = fopen(file_path, "r");
+    if (!fp) {
+        perror("open: ");
+        exit(EXIT_FAILURE);
+    }
+
+    if (fseek(fp, 0, SEEK_END)) {
+        perror("couldn't read file: ");
+        fclose(fp);
+        exit(EXIT_FAILURE);
+    }
+
+    long file_size = ftell(fp);
+    if (file_size == -1) {
+        perror("couldn't read file size: ");
+        fclose(fp);
+        exit(EXIT_FAILURE);
+    }
+
+    rewind(fp);
+
+    if (DEBUG_TEST)
+        printf("FILE SIZE = %ld\n", file_size);
+
+    char *input = malloc(file_size + 1);
+    if (!input) {
+        perror("failed to alloc memory: ");
+        fclose(fp);
+        exit(EXIT_FAILURE);
+    }
+
+    int read_len = fread(input, 1, file_size, fp);
+
+    if (DEBUG_TEST)
+        printf("FREAD READ SIZE = %d\n", read_len);
+
+    if (read_len != file_size) {
+        if (ferror(fp))
+            printf("error reading %s\n", file_path);
+        else if (feof(fp)) {
+            printf("EOF found\n");
+            printf("number of characters read = %d\n", read_len);
+            printf("input = %.*s\n", read_len, input);
+        }
+        fclose(fp);
+        exit(EXIT_FAILURE);
+    }
+
+    input[file_size] = '\0';
+    fclose(fp);
+
+    return input;
+}
+
 void run(char *file_name)
 { // sizeof(char) is always the smallest
   // addressable unit (i.e. usually 1 byte)
@@ -20,12 +76,14 @@ void run(char *file_name)
     char *file_path = malloc(size);
 
     if (DEBUG_TEST)
-        printf("%zu\n", size);
+        printf("path size: %zu\n", size);
 
     sprintf(file_path, "./res/%s", file_name);
 
     if (DEBUG_TEST)
-        printf("%s\n", file_path);
+        printf("file_path: %s\n", file_path);
+
+    printf("%s\n", read_file(file_path));
 }
 
 int main(int argc, char *argv[])
